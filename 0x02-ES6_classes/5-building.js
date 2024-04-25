701a58e089dc648f7cf8d92a3ccf3e3a0bffa7ed
yassineelhafyane <yassineelhafyane99@gmail.com>
2024-04-26 00:10:48 +0200

    .

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-underscore-dangle */
+export default class Building {
+  constructor(sqft) {
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
